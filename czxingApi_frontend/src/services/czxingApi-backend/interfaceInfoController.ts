47c679fe25762e11045463b1ceee835ea0a2056c
fDu-Czxing <274109405@qq.com>
2023-05-03 23:36:29 +0800

    generate method

---
+++ czxingApi_frontend/src/services/czxingApi-backend/interfaceInfoController.ts
@@ -0,0 +1,108 @@
+// @ts-ignore
+/* eslint-disable */
+import { request } from '@umijs/max';
+
+/** addInterfaceInfo POST /api/InterfaceInfo/add */
+export async function addInterfaceInfoUsingPOST(
+  body: API.InterfaceInfoAddRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseLong_>('/api/InterfaceInfo/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** deleteInterfaceInfo POST /api/InterfaceInfo/delete */
+export async function deleteInterfaceInfoUsingPOST(
+  body: API.DeleteRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseBoolean_>('/api/InterfaceInfo/delete', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** editInterfaceInfo POST /api/InterfaceInfo/edit */
+export async function editInterfaceInfoUsingPOST(
+  body: API.InterfaceInfoUpdateRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseBoolean_>('/api/InterfaceInfo/edit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** getInterfaceInfoById GET /api/InterfaceInfo/get */
+export async function getInterfaceInfoByIdUsingGET(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getInterfaceInfoByIdUsingGETParams,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseInterfaceInfo_>('/api/InterfaceInfo/get', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/** listInterfaceInfoByPage POST /api/InterfaceInfo/list/page */
+export async function listInterfaceInfoByPageUsingPOST(
+  body: API.InterfaceInfoQueryRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponsePageInterfaceInfo_>('/api/InterfaceInfo/list/page', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** listMyInterfaceInfoByPage POST /api/InterfaceInfo/my/list/page */
+export async function listMyInterfaceInfoByPageUsingPOST(
+  body: API.InterfaceInfoQueryRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponsePageInterfaceInfo_>('/api/InterfaceInfo/my/list/page', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** updateInterfaceInfo POST /api/InterfaceInfo/update */
+export async function updateInterfaceInfoUsingPOST(
+  body: API.InterfaceInfoUpdateRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseBoolean_>('/api/InterfaceInfo/update', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
